38f20ea47c436556398e51e15c69407e7a75ebf6
andreaortuno <andreaortuno@users.noreply.github.com>
2019-05-14 23:11:34 -0400

    added files

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'pyOpenBCI',         # How you named your package folder (MyLib)
+  packages = ['pyOpenBCI'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A lib for controlling OpenBCI devices',   # Give a short description about your library
+  author = 'OpenBCI, Inc.',                   # Type in your name
+  author_email = 'contact@openbci.com',      # Type in your E-Mail
+  url = 'https://github.com/OpenBCI/OpenBCI_Python/blob/Andrea/setup.py',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/andreaortuno/pyOpenBCI/archive/0.1.tar.gz',    # I explain this later on
+  keywords = ['device', 'control', 'eeg', 'emg', 'ekg', 'ads1299', 'openbci', 'ganglion', 'cyton', 'wifi'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pyserial',
+          'bitstring',
+          'urllib2',
+          'xmltodict',
+          'requests',
+      ]+ ["bluepy >= 2.0"] if sys.platform.startswith("linux") else [],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
