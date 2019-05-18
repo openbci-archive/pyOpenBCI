a36cbecc9df4a4c101bdcaab2cc79ecd48d84d16
andreaortuno <andreaortuno@users.noreply.github.com>
2019-05-18 10:28:21 -0400

    Update cyton.py

--- pyOpenBCI/cyton.py
+++ pyOpenBCI/cyton.py
@@ -33,7 +33,7 @@ class OpenBCICyton(object):
         max_packets_skipped: An integer specifying how many packets can be dropped before attempting to reconnect.
 
     """
-    def __init__(self, port=None, daisy=False, baud=115200, timeout=None, max_packets_skipped=10):
+    def __init__(self, port=None, daisy=False, baud=115200, timeout=None, max_packets_skipped=None):
         self.baud = baud
         self.timeout = timeout
         self.daisy = daisy
@@ -134,7 +134,7 @@ class OpenBCICyton(object):
         time.sleep(0.5)
         self.streaming = True
 
-    def check_connection(self, interval=2, max_packets_skipped=10):
+    def check_connection(self, max_packets_skipped=0, interval=2):
         """Verifies if the connection is stable. If not, it attempts to reconnect to the board"""
         if not self.streaming:
             print('Not streaming')
@@ -249,7 +249,7 @@ class OpenBCICyton(object):
             callback = [callback]
 
         # checks connection
-        self.check_connection()
+        self.check_connection(max_packets_skipped=self.max_packets_skipped)
 
         while self.streaming:
 
